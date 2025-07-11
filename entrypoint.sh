849a0578c23e2770a5c3e8bc14d8f29fad68b9be
BobNop6 <lunmister84@gmail.com>
2025-07-11 18:01:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,56 @@
 #!/usr/bin/env bash
 
-# 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+# === CONFIGURATION ===
+# Set your UUID and WebSocket paths
+UUID='9a35d517-f2c3-48f1-bf74-2c5f3844aa07'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
+
+# Optional: Nezha Probe Settings (leave blank if unused)
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
-sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
-sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
 
-# 设置 nginx 伪装站
-rm -rf /usr/share/nginx/*
-wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
-unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
-rm -f /usr/share/nginx/mikutap.zip
+# === UPDATE CONFIGS ===
+# Replace placeholders in config.json and nginx.conf
+sed -i "s#UUID#${UUID}#g" /etc/v2ray/config.json
+sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g" /etc/v2ray/config.json
+sed -i "s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/v2ray/config.json
+sed -i "s#TROJAN_WSPATH#${TROJAN_WSPATH}#g" /etc/v2ray/config.json
+sed -i "s#SS_WSPATH#${SS_WSPATH}#g" /etc/v2ray/config.json
 
-# 伪装 xray 执行文件
-RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv xray ${RELEASE_RANDOMNESS}
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-cat config.json | base64 > config
-rm -f config.json
+sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#TROJAN_WSPATH#${TROJAN_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
+# === RANDOMIZE XRAY BINARY NAME ===
+RANDOM_BIN=$(tr -dc 'a-z0-9' </dev/urandom | head -c 8)
+mv /usr/local/bin/xray "/usr/local/bin/${RANDOM_BIN}"
 
+# Download latest geoip and geosite data
+wget -q -O /etc/v2ray/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -q -O /etc/v2ray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+
+# === STATIC WEB CONTENT ===
+# Replace default nginx www content with camouflage site
+rm -rf /usr/share/nginx/html/*
+wget -q https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /tmp/mikutap.zip
+unzip -o /tmp/mikutap.zip -d /usr/share/nginx/html/
+rm -f /tmp/mikutap.zip
+
+# === OPTIONAL: Install Nezha Probe ===
+if [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]]; then
+    wget -qO nezha.sh https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh
+    chmod +x nezha.sh
+    ./nezha.sh install_agent "${NEZHA_SERVER}" "${NEZHA_PORT}" "${NEZHA_KEY}"
+fi
+
+# === START SERVICES ===
+# Start Nginx (reverse proxy)
 nginx
-base64 -d config > config.json
-./${RELEASE_RANDOMNESS} -config=config.json
+
+# Run Xray with updated config
+"/usr/local/bin/${RANDOM_BIN}" -config /etc/v2ray/config.json
